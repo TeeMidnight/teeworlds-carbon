814f57719962d31a63e0d96501656567e67d3fe7
Robert Müller <robytemueller@gmail.com>
2023-06-14 18:58:18 +0200

    Fix misc-redundant-expression
    /home/runner/work/teeworlds/teeworlds/src/base/system.c:2235:65: error: operator has equivalent nested operands [misc-redundant-expression,-warnings-as-errors]
                            || *str == '/' || *str == '\\' || *str == '|' || *str == '?' || *str == '*')
                               ~~~~~~~~~~~                                               ^

--- src/base/system.c
+++ src/base/system.c
@@ -2642,7 +2642,7 @@ char* str_sanitize_filename(char* aName)
 	{
 		// replace forbidden characters with a whispace
 		if(*str == '/' || *str == '<' || *str == '>' || *str == ':' || *str == '"'
-			|| *str == '/' || *str == '\\' || *str == '|' || *str == '?' || *str == '*')
+			|| *str == '\\' || *str == '|' || *str == '?' || *str == '*')
  			*str = ' ';
 		str++;
 	}
